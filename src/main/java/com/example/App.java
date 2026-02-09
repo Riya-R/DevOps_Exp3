c35b10ec1c2580c021a864de3874c869b885c3be
Riya-R <riya12.gawde@gmail.com>
2026-02-09 09:34:01 +0000

    1

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,14 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        int a = 10;
+        int b = 20;
+
+        int sum = a + b;
+
+        System.out.println("Sum of a and b is: " + sum);
     }
 }
